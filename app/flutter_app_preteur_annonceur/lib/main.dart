84e589922c3c74ebf5307fd58741df714a44f670
w136226 <steven.marmion@worldline.com>
2024-03-14 12:21:11 +0100

    Mise en place de la connexion Supabase

--- app/flutter_app_preteur_annonceur/lib/main.dart
+++ app/flutter_app_preteur_annonceur/lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_preteur_annonceur/src/app.dart';
+// ignore: depend_on_referenced_packages
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  await Supabase.initialize(
+    url: 'https://meubemsketmbaswvbxgb.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ldWJlbXNrZXRtYmFzd3ZieGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0MTQ3MjAsImV4cCI6MjAyNTk5MDcyMH0.eYfLjAuud9ea8sCPt9XETUPdbglymnBwAcXmDEhyJGs',
+  );
+  runApp(const MyApp());
 }
